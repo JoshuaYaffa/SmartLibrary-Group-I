3842b5d9c8c0e9d39f63891baf6e0605c8a8e713
JoshuaLUCT <joshuayaffa3@gmail.com>
2025-10-08 18:51:21 +0000

    Add basic data structures to operations.py

--- operations.py
+++ operations.py
@@ -1,1 +1,12 @@
- 
+"""
+ReadEasy Mini Library Management System
+Core operations module
+"""
+
+# Global data structures
+GENRES = ("Fiction", "Non-Fiction", "Sci-Fi", "Mystery", "Biography")
+books = {}  # Dictionary to store books
+members = []  # List to store members
+
+# We'll add our functions here in the next steps
+print("ReadEasy Library System - Operations module loaded") 
